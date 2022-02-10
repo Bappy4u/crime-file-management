1d620d604ec9de16ad952c38f6667aeae2ac1da6
Najmul H. Bappy <http.bappy@gmail.com>
2022-02-10 16:15:00 +0600

    Added template directory to the base dir and configured static dir

--- crimeFile/views.py
+++ crimeFile/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def loginView(request):
+    if request.user.is_authenticated:
+        return HttpResponse("<h1>You are already logged in</h1>")
     return render(request, "index.html")
