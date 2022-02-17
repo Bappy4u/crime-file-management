06b41715818cb4a5d8ed395e3eefeecf6f62b075
Najmul H. Bappy <http.bappy@gmail.com>
2022-02-17 16:57:57 +0600

    final base.html with sidebar and menubar

--- front-end/files/base.js
+++ front-end/files/base.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+    $("#menu-collapse-button").toggleClass("bx-align-left bx-x");
+  });
 });
