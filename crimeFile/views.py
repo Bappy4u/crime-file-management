a25c7fc95a4f7acba480d87272b4689398d9ecd0
Najmul H. Bappy <http.bappy@gmail.com>
2022-02-17 17:30:33 +0600

    added static files to the django and added the base template to the django

--- crimeFile/views.py
+++ crimeFile/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def loginView(request):
     if request.user.is_authenticated:
         return HttpResponse("<h1>You are already logged in</h1>")
-    return render(request, "index.html")
+    return render(request, "login.html")
+
+def FileHistoryView(request):
+    if request.user.is_authenticated:
+        return HttpResponse("<h1>You are already logged in</h1>")
+    return render(request, "file-history.html")
+
