a35dd560fe2a6ad166f544d43e08a8bd5b5a99af
Najmul H. Bappy <http.bappy@gmail.com>
2022-02-19 17:12:54 +0600

    Crime file login update

--- crimeFile/urls.py
+++ crimeFile/urls.py
@@ -1,7 +1,8 @@
 
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FileHistoryView)
+    path('', views.FileHistoryView, name="fileHisotyView")
 ]
