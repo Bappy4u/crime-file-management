fbc0549f39ecedadab3b816bd502dc31be4a2d54
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-01 20:37:19 +0600

    Wanted person app models initialized

--- wantedPerson/models.py
+++ wantedPerson/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class wantedPerson(models.model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    fathersName = models.CharField(max_length=255)
+    reason = models.CharField(max_length=255)
+    #photo = models.imageField()
+    nid_birthid = models.CharField(max_length=255)
