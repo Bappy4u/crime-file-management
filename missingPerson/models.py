3c01593f247c947953d10569374f923803e2c145
Najmul H. Bappy <http.bappy@gmail.com>
2022-05-18 17:18:43 +0600

    missing person update

--- missingPerson/models.py
+++ missingPerson/models.py
@@ -6,18 +6,22 @@ from django.conf import settings
 class MissingPerson(models.Model):
     STATUS_FOUND = "FOUND"
     STATUS_MISSING = "MISSING"
+    STATUS_PENDING = "PENDING"
     STATUS_CHOICES = [
         (STATUS_FOUND, 'Found'),
         (STATUS_MISSING, 'Missing'),
+        (STATUS_PENDING, 'Pending')
     ]
         
 
     name = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='missing-person', null=True)
+    description = models.CharField(max_length=500 , null=True)
     street = models.CharField(max_length=255)
     village = models.CharField(max_length=255)
     city = models.CharField(max_length=255, null=False)
     date = models.DateTimeField()
     birthDate = models.DateField(null=True)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=7, default=STATUS_MISSING)
+    contact = models.CharField(max_length=255, null=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=7, default=STATUS_PENDING)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
--- missingPerson/views.py
+++ missingPerson/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MissingPerson
 
 def missingPerson(request):
@@ -11,4 +11,19 @@ def missingPerson(request):
 
 
 def addMissingPerson(request):
+    if request.method == 'POST':
+           
+            user = request.user
+            name = request.POST['name']
+            street = request.POST['street']
+            village = request.POST['village']
+            city = request.POST['city']
+            description = request.POST['description']
+            phone = request.POST['phone']
+            photo = request.FILES.get('photo')
+            date = request.POST['date']
+            bdate = request.POST['bdate']
+            person = MissingPerson.objects.create(name=name, birthDate=bdate, user=user, street=street,village=village,contact=phone, city=city, date = date, description=description, photo = photo)
+            person.save()
+            return redirect('missingPerson')
     return render(request, 'add-missing-person.html')
